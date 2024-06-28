accd821eb30e7c8b48f349d34d57078f51f04150
Thomas Tumiel <ttumiel@gmail.com>
2024-06-28 21:56:04 +0100

    refactor trainer, add compile and epoch options

--- mingpt/trainer.py
+++ mingpt/trainer.py
@@ -8,24 +8,29 @@ from collections import defaultdict
 
 import torch
 from torch.utils.data.dataloader import DataLoader
+
+from mingpt.logger import Logger
 from mingpt.utils import CfgNode as CN
+from mingpt.utils import try_auto_cast
+
 
 class Trainer:
 
     @staticmethod
     def get_default_config():
         C = CN()
+        C.epochs = 1
         # device to train on
         C.device = 'auto'
         # dataloder parameters
         C.num_workers = 4
         # optimizer parameters
-        C.max_iters = None
         C.batch_size = 64
         C.learning_rate = 3e-4
         C.betas = (0.9, 0.95)
         C.weight_decay = 0.1 # only applied on matmul weights
         C.grad_norm_clip = 1.0
+        C.compile = False
         return C
 
     def __init__(self, config, model, train_dataset):
@@ -34,6 +39,7 @@ class Trainer:
         self.optimizer = None
         self.train_dataset = train_dataset
         self.callbacks = defaultdict(list)
+        self.logger = Logger()
 
         # determine the device we'll train on
         if config.device == 'auto':
@@ -41,6 +47,8 @@ class Trainer:
         else:
             self.device = config.device
         self.model = self.model.to(self.device)
+        if config.compile:
+            self.model = torch.compile(self.model)
         print("running on device", self.device)
 
         # variables that will be assigned to trainer class later for logging and etc
@@ -67,9 +75,9 @@ class Trainer:
         # setup the dataloader
         train_loader = DataLoader(
             self.train_dataset,
-            sampler=torch.utils.data.RandomSampler(self.train_dataset, replacement=True, num_samples=int(1e10)),
-            shuffle=False,
+            shuffle=True,
             pin_memory=True,
+            drop_last=True,
             batch_size=config.batch_size,
             num_workers=config.num_workers,
         )
@@ -77,33 +85,23 @@ class Trainer:
         model.train()
         self.iter_num = 0
         self.iter_time = time.time()
-        data_iter = iter(train_loader)
-        while True:
+        for epoch in range(config.epochs):
+            self.epoch = epoch
+            for batch in train_loader:
+                batch = [t.to(self.device) for t in batch]
 
-            # fetch the next batch (x, y) and re-init iterator if needed
-            try:
-                batch = next(data_iter)
-            except StopIteration:
-                data_iter = iter(train_loader)
-                batch = next(data_iter)
-            batch = [t.to(self.device) for t in batch]
-            x, y = batch
+                # forward the model
+                with try_auto_cast(self.device):
+                    logits, self.loss = model(*batch)
 
-            # forward the model
-            logits, self.loss = model(x, y)
+                # backprop and update the parameters
+                model.zero_grad(set_to_none=True)
+                self.loss.backward()
+                torch.nn.utils.clip_grad_norm_(model.parameters(), config.grad_norm_clip)
+                self.optimizer.step()
 
-            # backprop and update the parameters
-            model.zero_grad(set_to_none=True)
-            self.loss.backward()
-            torch.nn.utils.clip_grad_norm_(model.parameters(), config.grad_norm_clip)
-            self.optimizer.step()
-
-            self.trigger_callbacks('on_batch_end')
-            self.iter_num += 1
-            tnow = time.time()
-            self.iter_dt = tnow - self.iter_time
-            self.iter_time = tnow
-
-            # termination conditions
-            if config.max_iters is not None and self.iter_num >= config.max_iters:
-                break
+                self.trigger_callbacks('on_batch_end')
+                self.iter_num += 1
+                tnow = time.time()
+                self.iter_dt = tnow - self.iter_time
+                self.iter_time = tnow
